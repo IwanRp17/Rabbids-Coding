20db047c4805887f4200999cf8044c6116a0c199
IwanRp17 <iwan.rp.gd1@gmail.com>
2023-03-20 11:03:53 +0700

    Add Penyiramtanaman

---
+++ Penyiramtanaman.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "InteractableObject.h"
+#include "Penyiramtanaman.h"
+
+using namespace std;
+
+namespace RabbidsCoding
+{
+    void Penyiramtanaman:: interact()
+    {
+        cout << "Sucked"<<endl;    
+    }
+}
---
+++ Penyiramtanaman.h
@@ -0,0 +1,6 @@
+#include<iostream>
+#include "InteractableObject.h"
+
+class Penyiramtanaman: public InteractableObject{
+    public: void interact();
+};
